2a9aa194ab39b30b45922b8ad67357d2ea6d0880
JDovalina <132471849+JDovalina@users.noreply.github.com>
2025-02-18 10:42:18 -0600

    Update ListNode.java

--- ListNode.java
+++ ListNode.java
@@ -1,42 +1,41 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
+//Name - Jacob Dovalina
 
-public class ListNode implements Linkable
-{
-	private Comparable listNodeValue;
-	private ListNode nextListNode;
+public class ListNode implements Linkable {
+    private Comparable listNodeValue; // Value stored in the node
+    private ListNode nextListNode; // Reference to the next node
 
-	public ListNode()
-	{
-		listNodeValue = null;
-		nextListNode = null;
-	}
+    // Default constructor - initializes to null
+    public ListNode() {
+        listNodeValue = null;
+        nextListNode = null;
+    }
 
-	public ListNode(Comparable value, ListNode next)
-	{
-		listNodeValue=value;
-		nextListNode=next;
-	}
+    // Constructor that sets the value and next node
+    public ListNode(Comparable value, ListNode next) {
+        listNodeValue = value;
+        nextListNode = next;
+    }
 
-	public Comparable getValue()
-	{
-		return listNodeValue;
-	}
+    // Returns the value stored in this node
+    public Comparable getValue() {
+        return listNodeValue;
+    }
 
-	public ListNode getNext()
-	{
-	   return nextListNode;
-	}
+    // Returns the next node in the list
+    public ListNode getNext() {
+        return nextListNode;
+    }
 
-	public void setValue(Comparable value)
-	{
-		listNodeValue = value;
-	}
+    // Sets the value of this node
+    public void setValue(Comparable value) {
+        listNodeValue = value;
+    }
 
-	public void setNext(Linkable next)
-	{
-		nextListNode = (ListNode)next;
-	}
+    // Sets the next node in the list
+    public void setNext(Linkable next) {
+        nextListNode = (ListNode) next;
+    }
 }
